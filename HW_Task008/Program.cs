ec7ce432851aaf30c215b5b9d92a53a68dfbc9e5
Aleksandr <aavoron@list.ru>
2022-12-01 20:27:41 +0500

    HW_Task008 Completed

---
+++ HW_Task008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string strNumber = "2";
+string outLine = strNumber;
+if (number < 2){
+    outLine = "Нет решения.";
+};
+for (int i=4; i<=number; i+=2){
+    strNumber = i.ToString();
+    outLine = ($"{outLine}, {strNumber}");
+}
+Console.WriteLine(outLine);
