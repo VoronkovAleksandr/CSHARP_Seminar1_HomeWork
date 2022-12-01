117520864ce0dd144cc3f462285796b04804a6ee
Aleksandr <aavoron@list.ru>
2022-12-01 20:07:52 +0500

    HW_Task006 Completed

---
+++ HW_Task006/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и 
+// выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.Write("Введите число для проверки на четность: ");
+int firstNumber = int.Parse(Console.ReadLine());
+string answer = "Нет";
+int result = firstNumber % 2;
+if (result == 0){
+    answer = "Да";
+};
+Console.WriteLine(answer);
